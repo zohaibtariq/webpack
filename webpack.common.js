c199cc05add6389da1287541b3a6e91cf58d2d88
Zohaib Tariq <zohaib.tariq@arpatech.com>
2019-08-21 14:44:04 +0500

    seperate dev and prod environment setup dev server and cache at prod environment.

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,12 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode: "production",/*development*/
-    // mode: "development",/*production*/
-    /*devtool: "none",*/
     entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
     module: {
         rules: [
             {
