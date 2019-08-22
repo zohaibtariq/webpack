33e8d02b386c9b65bb9c0d5612628ecba9b623f1
Zohaib Tariq <zohaib.tariq@arpatech.com>
2019-08-22 17:28:27 +0500

    minify images via different plugins loss less and lossy.

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+const ImageminPlugin = require("imagemin-webpack");
 module.exports = {
     entry: {
         main: "./src/index.js",
@@ -13,18 +14,50 @@ module.exports = {
             },
             {
                 test: /\.(svg|png|jpe?g|jpeg|gif)$/,
-                use: {
-                    loader: "file-loader",   // load files
-                    options: {
-                        name(file) {
-                            if (process.env.NODE_ENV === 'development')
-                                return '[path][name].[ext]';
-                            return '[contenthash].[ext]';
-                        },
-                        outputPath: "images"
+                use: [
+                    {
+                        loader: "file-loader",   // load files
+                        options: {
+                            name(file) {
+                                if (process.env.NODE_ENV === 'development')
+                                    return '[path][name].[ext]';
+                                return '[contenthash].[ext]';
+                            },
+                            outputPath: "images"
+                        }
                     }
-                }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        // Make sure that the plugin is after any plugins that add images, example `CopyWebpackPlugin`
+        new ImageminPlugin({
+            bail: false, // Ignore errors on corrupted images
+            cache: true,
+            imageminOptions: {
+                // Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
+
+                // Lossless optimization with custom option
+                // Feel free to experiment with options for better result for you
+                plugins: [
+                    ["mozjpeg"],
+                    ["pngquant"],
+                    ["gifsicle", { interlaced: true }],
+                    ["jpegtran", { progressive: true }],
+                    ["optipng", { optimizationLevel: 5 }],
+                    [
+                        "svgo",
+                        {
+                            plugins: [
+                                {
+                                    removeViewBox: false
+                                }
+                            ]
+                        }
+                    ]
+                ]
+            }
+        })
+    ]
 };
